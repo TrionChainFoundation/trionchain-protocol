65a3bbcc6cd8f6ccb12a2843cec2815b07c54b99
jorge pumar <jorgepumar@jorges-MacBook-Pro.local>
2026-01-03 21:24:47 -0400

    fix: Update Dashboard to connect to DigitalOcean VPS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
     const connect = async () => {
       try {
         setStatus("🟡 CONNECTING...");
-        const provider = new WsProvider('ws://127.0.0.1:9944');
+        const provider = new WsProvider('ws://174.138.65.175:9944');
         const newApi = await ApiPromise.create({ provider });
         setStatus("🟢 LIVE TRACKING");
 
